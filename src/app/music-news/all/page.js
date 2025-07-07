81b432d6e255e017a2e4e84b1b22257d30c3aaf2
KenjiTadano <tadanokenji5222@gmail.com>
2025-07-07 16:44:27 +0900

    詳細ページから戻るボタン押すと１ページ目に戻ってしまう不具合を修正

--- src/app/music-news/all/page.js
+++ src/app/music-news/all/page.js
@@ -1,7 +1,7 @@
 // app/music-news/all/page.js
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MusicNewsList from "../MusicNewsList";
 import { useRouter } from "next/navigation";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
@@ -11,13 +11,23 @@ import Typography from "@mui/material/Typography";
 import { Container, Pagination } from "@mui/material";
 
 const PAGE_SIZE = 10; // 1ページあたりのニュース数
+const LAST_PAGE_KEY = "lastMusicNewsPage"; // localStorage のキー
 
 const AllMusicNewsPage = () => {
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1); // 現在のページ番号
 
+  useEffect(() => {
+    // localStorage からページ番号を読み込む
+    const lastPage = localStorage.getItem(LAST_PAGE_KEY);
+    if (lastPage) {
+      setCurrentPage(parseInt(lastPage, 10));
+    }
+  }, []);
+
   const handlePageChange = (event, value) => {
     setCurrentPage(value);
+    localStorage.setItem(LAST_PAGE_KEY, value.toString()); // ページ変更時に localStorage に保存
   };
 
   return (
